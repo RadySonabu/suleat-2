1b85cc3ef0b457ef021621e2aae6f52654516804
RadySonabu <ardynubanos@gmail.com>
2024-03-14 14:12:51 +0800

    Updated forms page

--- src/app/forms/[[...page-name]]/page.jsx
+++ src/app/forms/[[...page-name]]/page.jsx
@@ -1,11 +1,20 @@
+'use client'
 import React from 'react'
-import { useRouter } from 'next/router'
+import {useRouter, usePathname, useSearchParams} from 'next/navigation'
+import WaitlistForm from '@/components/forms/waitlist'
+import ProfileForm from '@/components/forms/profile'
 function FormPage() {
-    const router = useRouter()
+    const searchParams = useSearchParams()
+
+    const dictForms = {
+      'waitlist': <WaitlistForm/>,
+      'profile': <ProfileForm/>
+    }
+    console.log(dictForms[searchParams])
+    const searchValue = searchParams.toString().replace('=', '')
   return (
     <>
-    FormPage 
-    {router.query.slug}
+      {dictForms[searchValue]}
     </>
   )
 }
---
+++ src/components/forms/waitlist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WaitlistForm() {
+  return (
+    <div>WaitlistForm</div>
+  )
+}
+
+export default WaitlistForm
